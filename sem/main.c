fa7a1c70b2aaaf4a15b4e4f94a2ad4f3bb3113d4
BartoszMakowski <baboshpl@gmail.com>
2016-02-25 21:04:27 +0100

    dodano komentarze do kodu

--- sem/main.c
+++ sem/main.c
@@ -1,13 +1,29 @@
+//Bartosz Makowski
+
+/*
+Rozwiazanie wykorzystuje sposrod mechanizmow IPC wylacznie semafory.
+Kluczowe jest spostrzezenie, ze jesli kazdy z filozofow podnosi sztucce w takiej samej
+kolejnosci (lewa-prawa/prawa-lewa), to do zakleszczenia moze dojsc wylacznie wtedy,
+gdy wszyscy filozofowie podniosa widelec po tej samej stronie kazdego z nich.
+Nalezy wiec zapewnic, by w dowolnej chwili liczba filozofow, ktorzy probuja podniesc sztucce,
+nie byla wieksza od N-1, co gwarantuje odpowiedni semafor.
+ 
+*/
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-#define N 5
+#define N 5 // liczba filozofow
+
+void wypisz_stan(int, char); // wypisywanie aktualnego stanu jednego filozofa
+void filozof(int); // proces filozofa
 
 int sid_widelce, sid_dostep;
 struct sembuf op_wid, op_dost;
+
 
 void wypisz_stan(int k, char stan){
 	int i;
@@ -18,43 +34,39 @@ void wypisz_stan(int k, char stan){
 }
 
 void filozof(int k){
-	int lewy, prawy;
+	int lewy, prawy; // indeksy sztuccow po obu stronach filozofa
 	lewy = k;
 	prawy = (k + 1 ) % N;
-	int i;
 	if(fork()==0){
 		while(1){
-			//printf("%d: MYŚLI\n", k);
-			wypisz_stan(k, '-');
-	
-			sleep(rand()%3);
-			//printf("%d: ZGŁODNIAŁ\n", k);
-			wypisz_stan(k, 'o');
 			
-			op_dost.sem_op = -1;
-			semop(sid_dostep, &op_dost, 1);
+			wypisz_stan(k, '-');	
+			sleep(rand()%3); // filozof mysli
+			
+			wypisz_stan(k, 'o'); // filozof chce jesc
+			
+			op_dost.sem_op = -1; 
+			semop(sid_dostep, &op_dost, 1); // sprawdzanie, czy mozna probowac podnosic sztucce - gwarancja braku zakleszczenia
 			
 			op_wid.sem_num = prawy;
 			op_wid.sem_op = -1;
-			semop(sid_widelce, &op_wid, 1);			
+			semop(sid_widelce, &op_wid, 1); // podniesienie prawego stucca
 
 			op_wid.sem_num = lewy;
 			op_wid.sem_op = -1; 
-			semop(sid_widelce, &op_wid, 1);
+			semop(sid_widelce, &op_wid, 1); // podniesienie lewego sztucca
 			
-			//printf("%d: JE\n", k);
 			wypisz_stan(k, 'X');
-
-			sleep(rand() %4);
+			sleep(rand() %4); // filozof je
 			
 			op_wid.sem_op = 1;
-			semop(sid_widelce, &op_wid, 1);
+			semop(sid_widelce, &op_wid, 1); // zwolnienie lewego sztucca
 			
 			op_wid.sem_num = prawy;
-			semop(sid_widelce, &op_wid, 1);
+			semop(sid_widelce, &op_wid, 1); // zwolnienie prawego sztucca
 
 			op_dost.sem_op = 1;
-			semop(sid_dostep, &op_dost, 1);
+			semop(sid_dostep, &op_dost, 1); // zwolnienie miejsca filozofa
 		
 		}
 	}
@@ -74,13 +86,9 @@ int main(int args, char *argv[]){
 	semctl(sid_dostep, 0, SETVAL, N-1);
 	
 	for(i=0; i<N; i++){
-		semctl(sid_widelce, i, SETVAL, 1);
+		semctl(sid_widelce, i, SETVAL, 1); 
 		filozof(i);
 	}
-
-
-	
-	
-
+	return(EXIT_SUCCESS);
 }
 
--- sem_shm/main.c
+++ sem_shm/main.c
@@ -1,3 +1,4 @@
+//Bartosz Makowski
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -10,74 +11,65 @@
 #include <signal.h>
 #include <sys/ipc.h>
 
-int *stan;
-int sid, sid2, mid;
-struct sembuf op, op2;
+void odloz_widelec(int); // zwalnianie sztuccow
+void testuj(int); // sprawdzanie dostepnosci sztuccow
+void filozof(int); // proces filozofa
 
-void podnies_widelec(int k){
-	stan[k] = 1;
-	testuj(k);
-	if(stan[k]!=2){	
-            struct sembuf op2;
-            op2.sem_num = k;
-            op2.sem_flg = 0;
-            op2.sem_op = -1;
-            semop(sid, &op2, 1);
-	}	
-}
+
+int *stan;
+int sid_filozof, sid_dostep, mid;
+struct sembuf op_fil, op_dost;
+
 
 void odloz_widelec(int k){
+// jesli filozow konczy jesc, sprawdza, czy zwolnione sztucce nie sa potrzebne sasiadom
 	stan[k] = 0;
 	testuj((k+4)%5);
 	testuj((k+1)%5);	
 }
 
 void testuj(int k){
-
-	if ((stan[(k+4)%5] != 2) && (stan[(k+1)%5] != 2) && (stan[k] == 1)){
+// jesli sasiedzi nie jedza i filozof chce jesc, to nie ma przeszkod, by to uczynil
+	if ((stan[(k+4)%5] != 2) && (stan[(k+1)%5] != 2) && (stan[k] == 1)){ 
 		stan[k]=2;
-		op.sem_num = k;
-        op.sem_flg = 0;
-        op.sem_op = 1;
-		semop(sid, &op, 1);
+		op_fil.sem_num = k;
+        op_fil.sem_flg = 0;
+        op_fil.sem_op = 1;
+		semop(sid_filozof, &op_fil, 1); // zgoda na jedzenie
 	}
 }
 
 void filozof(int k){
     if(fork()==0){
         while(1){
-            //printf("FILOZOF %d MYŚLI.\n", k);
-            sleep(rand()%3);
-            //printf("FILOZOF %d JEST GŁODNY.\n", k);
-
-			op2.sem_num = 0;
-            op2.sem_flg = 0;
-            op2.sem_op = -1;
-			semop(sid2, &op2, 1);
-
+            sleep(rand() %3); // filozof mysli
+			
+			op_dost.sem_num = 0; 
+            op_dost.sem_flg = 0;
+            op_dost.sem_op = -1;
+			semop(sid_dostep, &op_dost, 1); // wejscie do sekcji krytycznej
 			stan[k]=1;
 			testuj(k);
 
-			op2.sem_num = 0;
-            op2.sem_flg = 0;
-            op2.sem_op = 1;
-			semop(sid2, &op2, 1);
+			op_dost.sem_num = 0; 
+            op_dost.sem_flg = 0;
+            op_dost.sem_op = 1;
+			semop(sid_dostep, &op_dost, 1); // wyjscie z sekcji krytycznej
 
-			op.sem_num = k;
-        	op.sem_flg = 0;
-        	op.sem_op = -1;
-			semop(sid, &op, 1);
+			op_fil.sem_num = k; 
+        	op_fil.sem_flg = 0;
+        	op_fil.sem_op = -1;
+			semop(sid_filozof, &op_fil, 1); // proba jedzenia
+            
+            sleep(rand() %4); // filozof je
 
-            //printf("FILOZOF %d JE.\n", k);
-            sleep(rand()%3);
-
-			op2.sem_op = -1;
-			semop(sid2, &op2, 1);
+			op_dost.sem_op = -1; //
+			semop(sid_dostep, &op_dost, 1); // wejscie do sekcji krytycznej
 
             odloz_widelec(k);
 
-			op2.sem_op = 1;
-			semop(sid2, &op2, 1);
+			op_dost.sem_op = 1;
+			semop(sid_dostep, &op_dost, 1); // wyjscie z sekcji krytycznej
         }
     }
 }
@@ -85,42 +77,33 @@ void filozof(int k){
 
 int main(int args, char** argv) {
     
-    int i,k;    
-    sid = semget(0x123, 5, 0600 | IPC_CREAT);
-	sid2 = semget(0x124, 1, 0600 | IPC_CREAT);
+    int i;    
+    sid_filozof = semget(0x123, 5, 0600 | IPC_CREAT);
+	sid_dostep = semget(0x124, 1, 0600 | IPC_CREAT);
 	mid = shmget(0x125, 1024, 0600 | IPC_CREAT);
-    stan = (int *)shmat(mid, 0,0);
-	
+    stan = (int *)shmat(mid, 0,0); // tablica w pamieci wspoldzielonej, zawierajaca stan kazdego z filozofow
+	srand(time(NULL));
     
     for (i=0; i < 5; i++){
 		stan[i]=0;
-        semctl(sid, i, SETVAL, 0);
+        semctl(sid_filozof, i, SETVAL, 0);
     }
 
-	semctl(sid2, 0, SETVAL, 1);    
+	semctl(sid_dostep, 0, SETVAL, 1);    
 
     for (i=0; i < 5; i++){
         filozof(i);
     }
 
-	while(1){
-		
-		//for (i=0; i < 5; i++){
-       // 	k = semctl(sid, i, GETVAL, 0);
-		//	printf("%d: %d\n", i, k, stan[i]); 				
-		//}
+	while(1){		
+// czytelne wypisywanie ID: STAN
 		printf("\t\t\t%d: %d\n", 0, stan[0]);
 		printf("\t\t%d: %d", 1, stan[1]);  
 		printf("\t\t%d: %d\n", 4, stan[4]);
 		printf("\t\t  %d: %d", 2, stan[2]);  
-		printf("\t   %d: %d\n\n\n", 3, stan[3]);
-		//printf("W:\t%d\n", semctl(sid2, 0, GETVAL, 0));  
+		printf("\t   %d: %d\n\n\n", 3, stan[3]); 
 		sleep(1);
 	}
-    
-    wait(NULL);
-
-	
     
     exit(0);
     
